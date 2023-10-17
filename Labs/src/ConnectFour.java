8f2e7a3c11e5a00a24b6d3afc42a6c880a5a3d19
Aarya Bhorra <1302ab11@gmail.com>
2023-10-17 15:42:15 +0800

    implementation of enemy avoidance and obstacle overcoming

---
+++ Labs/src/ConnectFour.java
@@ -0,0 +1,17 @@
+class Connect4Agent{
+
+    /**
+     * The move method selects the column to drop the disc into, given the board and the symbol.
+     * @param board an array of Strings where each string represents a column of the board.
+     * @param symbol the symbol representing the agent's tokens.
+     * @param lastMove the lastMove your opponent played, or -1 if this is the first move of the game.
+     * @return, the column the player chooses, given the game state.
+     * **/
+    public int move(String[] board, char symbol, int lastMove){
+        //fill in code
+
+
+    }
+
+
+}
